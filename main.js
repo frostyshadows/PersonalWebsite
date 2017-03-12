200823fbfb4089b805f062a0d5eb9c2df1df9bcd
Sherry Yuan <frostyyshadows@gmail.com>
2017-03-11 18:56:47 -0800

    added slideshow

--- main.js
+++ main.js
@@ -27,4 +27,29 @@ $(function () {
         }, 800);
     });
 
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    $("button.display-left").click(function () {
+        plusDivs(-1)
+    });
+
+    $("button.display-right").click(function () {
+        plusDivs(1)
+    });
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex-1].style.display = "block";
+    }
 });
